8e93cfea65445b88dc16dcd096e9cbff9dd59cbf
BrownieAsd <129280045+BrownieAsd@users.noreply.github.com>
2023-05-11 20:41:09 +0200

    Add files via upload

---
+++ Polski.h
@@ -0,0 +1,44 @@
+//
+//  Polski.h
+//  Lab11
+//  Spolszczenie C++
+//  Created by Thomas Browne on 11/05/23.
+//
+
+#ifndef Polski_h
+#define Polski_h
+#define długi long
+#define unosić float
+#define prawdziwy true
+#define fałszywy false
+#define stały const
+#define znak char
+#define główny main
+#define póki while
+#define dla for
+#define jeśli if
+#define albo else
+#define cwyjscie cout
+#define cwejscie cin
+#define nowy new
+#define jednostka int
+#define zwróć return
+#define nicość void
+#define złam break
+#define przełącz switch
+#define przypadek case
+#define bul bool
+#define konieclinii endl
+#define domniejszego tolower
+#define dowiekszego toupper
+#define sznur string
+#define klasa class
+#define publiczny public
+#define prywatne private
+#define ochroniane protected
+#define dalej continue
+#define jeślistrumień ifstream
+#define zstrumień ofstream
+#define podwójny double
+
+#endif /* Polski_h */
